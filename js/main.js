1d0785305c853f70a65ec192b836aafa43d50790
Vitaly Polyakov <vpolyakov.mog@gmail.com>
2020-02-12 20:37:35 +0300

    Added a pop-up window when clicking on the button

--- js/main.js
+++ js/main.js
@@ -5,10 +5,24 @@ document.addEventListener('DOMContentLoaded', function() {
         headerBurger = document.querySelector('.header__burger');
 
     headerBurger.addEventListener('click', () => {
-
         headerMenu.classList.toggle('active');
         headerBurger.classList.toggle('active');
-
     });   
+
+    let 
+        cardsButtons = document.querySelectorAll('.header__btn'),
+        contentInfo = document.querySelector('.header__cardinfo'),
+        contentClose = document.querySelector('.header__close');
+
+    cardsButtons.forEach(item => {
+        item.addEventListener('click', () => {
+            contentInfo.classList.add('active');    
+        });
+    });
+
+    contentClose.addEventListener('click', () => {
+        contentInfo.classList.remove('active');
+
+    });
 
 });
